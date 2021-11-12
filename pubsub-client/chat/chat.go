5284fa03018897ff9cc97a0a755df1e468e6a9c4
Carlos Tenes <cctenes@gmail.com>
2021-11-11 23:11:15 -0600

    Pubsub-mongo

--- pubsub-client/chat/chat.go
+++ pubsub-client/chat/chat.go
@@ -3,6 +3,9 @@ package chat
 import (
 	"fmt"
 	"log"
+	"math/rand"
+	"strconv"
+	"strings"
 
 	//"net/http"
 	"cloud.google.com/go/pubsub"
@@ -22,6 +25,8 @@ type Server struct {
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
 	//implementar pubsub
 	fmt.Println("Received from grpc client: %s", message.Body)
+	sx := strings.Split(message.Body, "|")
+	x := setWiner(sx[0], sx[1], sx[2], sx[3])
 	//codigo de pubsub aqui :)
 	//ctx := context.Background()
 	client, err := pubsub.NewClient(ctx, "alpine-aspect-325702")
@@ -33,7 +38,7 @@ func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, erro
 	topic = client.Topic(topicName)
 
 	msg := &pubsub.Message{
-		Data: []byte(message.Body),
+		Data: []byte(x),
 	}
 
 	aaaa, errz := topic.Publish(ctx, msg).Get(ctx)
@@ -46,4 +51,68 @@ func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, erro
 
 
 	return &Message{Body: message.Body}, nil
+}
+
+func setWiner(name string, players string, game string, corr string) string {
+	//count++
+	//para el juego
+	intVar, _ := strconv.Atoi(players)
+	c := name + "|" + players + "|" + game + "|" + corr + "|" + calcGame(game, intVar)
+	return c
+}
+
+func calcGame(game string, players int) string {
+	switch game {
+	case "1":
+		return game1(players)
+	case "2":
+		return game2(players)
+	case "3":
+		return game3(players)
+	default:
+		return "1"
+
+	}
+	//return 1
+}
+
+func game1(players int) string {
+	pl1 := rand.Intn(players-1) + 1
+	pl2 := rand.Intn(players-1) + 1
+	pl3 := rand.Intn(players-1) + 1
+
+	mx := [3]int{pl1, pl2, pl3}
+
+	final := rand.Intn(len(mx))
+	return strconv.Itoa(mx[final])
+}
+
+func game2(players int) string {
+
+	mx := [5]int{
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+	}
+
+	final := rand.Intn(len(mx))
+	return strconv.Itoa(mx[final])
+}
+
+func game3(players int) string {
+	mx := [8]int{
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+		rand.Intn(players-1) + 1,
+	}
+
+	final := rand.Intn(len(mx))
+	return strconv.Itoa(mx[final])
 }
--- pubsub-worker/main.go
+++ pubsub-worker/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"cloud.google.com/go/pubsub"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"strconv"
+	"strings"
+	"time"
+
 	//"fmt"
 	"log"
 	"golang.org/x/net/context"
 )
 
+const (
+	URI = "mongodb://root:example@localhost:27017/"
+)
 
+type Data struct {
+	NumberReq int    `bson:"number_req" json:"number_req"`
+	Game     string `bson:"game" json:"game"`
+	NameGame string `bson:"name_game" json:"name_game"`
+	Winner  string `bson:"winner" json:"winner"`
+	Players int    `bson:"players" json:"players"`
+	Worker  string `bson:"worker" json:"worker"`
+}
 
 func main() {
-
+	clientM, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI(URI))
 	ctx := context.Background()
 	projID := "alpine-aspect-325702"
 
@@ -26,12 +44,48 @@ func main() {
 	sub, errz := client.CreateSubscription(context.Background(), "sopes-g", pubsub.SubscriptionConfig{Topic: topic})
 	errz = sub.Receive(context.Background(), func(ctx context.Context, m *pubsub.Message) {
 		log.Printf("Got message: %s", m.Data)
+		al := string(m.Data)
+		sx := strings.Split(al, "|")//valores spliteados
+		xx := convertToMongo(sx[0], sx[1], sx[2], sx[3], sx[4])
+		newDataMongo(xx, *clientM)
 		m.Ack()
 	})
 	if errz != nil {
 		// Handle error.
 	}
 
+}
+
+func convertToMongo(gameName string, players string, game string, numReq string, winner string) Data{
+	intVar, _ := strconv.Atoi(players)
+	x, _ := strconv.Atoi(numReq)
+	c := Data{
+		NumberReq: x,
+		Game:      game,
+		NameGame:  gameName,
+		Players:   intVar,
+		Worker:    "RabbitMQ",
+		Winner:    winner,
+	}
+
+	return c
+}
+
+func newDataMongo(data Data, c mongo.Client) bool {
+
+	col := c.Database("Proyecto2Sopes").Collection("Logs")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	//dt, _:= toDoc(data)
+	_, insertErr := col.InsertOne(ctx, data)
+	//fmt.Println(xx)
+	if insertErr != nil {
+		fmt.Println("InsertONE Error:", insertErr)
+		defer cancel()
+		return false
+	} else {
+		defer cancel()
+		return true
+	}
 }
 
 
