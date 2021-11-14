df0d1741d78474c60d936643c23e9f7ae862fe99
Carlos Tenes <cctenes@gmail.com>
2021-11-13 20:35:00 -0600

    Reportes de node, esqueleto react

--- node-app/index.js
+++ node-app/index.js
@@ -16,21 +16,73 @@ const collection = db.collection('Logs');
 
 //routes
 app.get('/', (req, res) => {
-    res.json({"id":'ct'})
+    res.json({ "id": 'ct' })
 })
 
 app.get('/dashboard', async (req, res) => {
     //calcular reportes
-    const f = await collection.find({}).toArray();
+    const f = {
+        "data_mongo": await allData(),
+        "worker_report": await workerReport(),
+        "top_3": await top3()
+    }
     //console.log(f)
-   res.json(f);
+    res.json(f);
+})
+
+app.get('/redis', async (req, res) => {
+    //calcular reportes
+    const f = {
+        "latest": await getLast10(),
+        "best": await best10()
+    }
+    //console.log(f)
+    res.json(f);
+})
+
+app.get('/user/:id', async (req, res) => {
+    //calcular reportes
+    const f = await userReport(req.params.id);
+    //console.log(f)
+    res.json(f);
 })
 
 //app listen
 app.listen(port, async () => {
-   
-    
+
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
 //helpers
+
+async function allData() {
+    const f = await collection.find({}).toArray();
+    return f;
+}
+
+async function top3() {
+    const c = await collection.aggregate([{ $group: { _id: "$game", count: { $sum: 1 } } }])
+    return c.toArray();
+}
+
+async function workerReport() {
+    // :)
+    const c = await collection.aggregate([{ $group: { _id: "$worker", count: { $sum: 1 } } }])
+    return c.toArray();
+}
+
+async function userReport(id){
+    //get games
+    const f = await collection.find({"winner":id}).toArray();
+    return f;
+}
+
+async function getLast10(){
+    const f = await collection.find({}).sort({_id:-1}).limit(10);
+    return f.toArray();
+}
+
+async function best10(){
+    const c = await collection.aggregate([{ $group: { _id: "$winner", count: { $sum: 1 } } }]).sort({_id:1}).limit(10);
+    return c.toArray();
+}
